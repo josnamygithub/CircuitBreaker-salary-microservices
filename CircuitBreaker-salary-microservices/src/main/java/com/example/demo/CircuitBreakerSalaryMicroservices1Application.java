1feabb5ae278739981b381fa05e38bf1db0b3f92
josnamygithub <131941980+josnamygithub@users.noreply.github.com>
2023-11-28 10:56:41 +0530

    Add files via upload

---
+++ CircuitBreaker-salary-microservices/src/main/java/com/example/demo/CircuitBreakerSalaryMicroservices1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerSalaryMicroservices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerSalaryMicroservices1Application.class, args);
+	}
+
+}
